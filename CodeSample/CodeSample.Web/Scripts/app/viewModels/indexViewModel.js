12f748556742ef4b53e051e00911f0997912f817
Andrew Cohen <omegaluz@gmail.com>
2013-09-05 21:36:29 -0500

    downgraded knockout and added crud editor

--- CodeSample/CodeSample.Web/Scripts/app/viewModels/indexViewModel.js
+++ CodeSample/CodeSample.Web/Scripts/app/viewModels/indexViewModel.js
@@ -6,18 +6,24 @@ app.viewModels = app.viewModels || {};
 app.viewModels['indexViewModel'] = (function (app) {
 
     var pages = ko.observableArray([]),
+        selectedPage = ko.observable(null),
         loadPages = function () {
             app.dataContext.getPages(loadPagesCallback);
         },
         loadPagesCallback = function (data) {
             pages(data);
+        },
+        addPage = function () {
+            selectedPage(new app.models.Page());
         };
 
     return {
         init: function () {
             loadPages();
         },
-        pages: pages
+        pages: pages,
+        selectedPage: selectedPage,
+        addPage: addPage
     };
 
 })(app);
