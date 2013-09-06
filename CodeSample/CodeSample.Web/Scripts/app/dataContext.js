f4b02c8a1bcf3ed7bb5083667f14fd9f8025eab1
Andrew Cohen <omegaluz@gmail.com>
2013-09-05 20:31:55 -0500

    set up basic index viewmodel

--- CodeSample/CodeSample.Web/Scripts/app/dataContext.js
+++ CodeSample/CodeSample.Web/Scripts/app/dataContext.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="../_references.js" />
+﻿/// <reference path="~/Scripts/_references.js" />
 
 var app = app || {};
 app['dataContext'] = (function (app) {
---
+++ CodeSample/CodeSample.Web/Scripts/app/viewModels/indexViewModel.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="~/Scripts/_references.js" />
+
+var app = app || {};
+app.viewModels = app.viewModels || {};
+
+app.viewModels['indexViewModel'] = (function (app) {
+
+
+
+    return {
+        init: function () {
+
+        }
+    };
+
+})(app);
