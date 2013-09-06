5c3dacf443e7788a2c6c9b071fb7bb473dbe332c
Andrew Cohen <omegaluz@gmail.com>
2013-09-05 19:38:41 -0500

    got rid of more default scaffolding

--- CodeSample/CodeSample.Web/Controllers/HomeController.cs
+++ CodeSample/CodeSample.Web/Controllers/HomeController.cs
@@ -15,18 +15,5 @@ namespace CodeSample.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
